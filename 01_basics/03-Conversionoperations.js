29304372251e5d167c6bb7ab69dc5ecffd9e23b7
Jatin Hasija <jatinhasija2@gmail.com>
2024-12-27 09:24:25 +0000

    Conversion of Datatypes

---
+++ 01_basics/03-Conversionoperations.js
@@ -0,0 +1,30 @@
+// Conversion
+let score = "33abc"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score) // String conversion in Number
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// Basic conversion
+// "33" to number = 33
+// "33abc" to number = NaN
+// true = 1; false = 0;
+
+let isLoggedIn = 1
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanisLoggedIn)
+
+// conversion
+// 1 = true; 0 = false;
+// "jatin" = 1
+// "" = 0
+
+
+// example of other conversion
+let someNumber = 11
+let numberInString = String(someNumber)
+console.log(numberInString)
+console.log(typeof numberInString)
