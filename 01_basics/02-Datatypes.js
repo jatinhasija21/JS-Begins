7f5933dfca1fe8e32d36ef4fbb6cef0dd118b0c4
Jatin Hasija <jatinhasija2@gmail.com>
2024-12-27 08:25:04 +0000

    Datatypes

---
+++ 01_basics/02-Datatypes.js
@@ -0,0 +1,26 @@
+// "use strict" // treat all js code as newer version or js.
+
+// alert(3 + 3) // not prefer to use alert bz we are using new js not browser.
+
+let name = "jatin"
+let age = 20
+
+// name and age is data types
+
+// data types: Premetive data types
+
+// number = 2 to power 53
+// bigint = for large number
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = not defined
+// symbol = used for uniqueness
+
+// objects
+
+console.log(typeof "jatin"); // string
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); // object
