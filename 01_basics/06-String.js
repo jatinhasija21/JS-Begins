053421c7945c3d4d4ef8b5cdacdf49cb6bf45e90
Jatin Hasija <jatinhasija2@gmail.com>
2025-01-02 06:30:19 +0000

    String and its functions

---
+++ 01_basics/06-String.js
@@ -0,0 +1,29 @@
+const name = "jatin"
+const surname = " hasija"
+
+// console.log(name + surname + " available this side") // Prefer not to use...
+
+console.log(`My name is ${name} and my surname is ${surname}`)
+
+// Second way of declare string in js.
+// Modern way of using string in js.
+
+const gameName = new String("GodHand On")
+
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(6))
+console.log(gameName.indexOf("d"))
+console.log(gameName.substring(0, 3))
+console.log(gameName.slice(-9, 3))
+console.log(gameName.includes('God'))
+console.log(gameName.split(' '))
+
+const anotherString = "   jatin   "
+console.log(anotherString)
+console.log(anotherString.trim())
+
+const url = "https://jatin.com/jatin%20hasija"
+console.log(url.replace('%20', '-'))
